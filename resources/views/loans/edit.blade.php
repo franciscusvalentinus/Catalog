b2200b5e25d211be4fde34c85462910d04a570c9
franciscusvalentinus <120277436+franciscusvalentinus@users.noreply.github.com>
2022-12-17 02:57:14 +0700

    update view

--- resources/views/loans/edit.blade.php
+++ resources/views/loans/edit.blade.php
@@ -1,21 +1,21 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Edit User
+            Edit Loan
         </h2>
     </x-slot>
 
     <div>
         <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="mt-5 md:mt-0 md:col-span-2">
-                <form method="post" action="{{ route('users.update', $user->id) }}">
+                <form method="post" action="{{ route('loans.update', $loan->id) }}">
                     @csrf
                     @method('put')
                     <div class="shadow overflow-hidden sm:rounded-md">
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <label for="name" class="block font-medium text-sm text-gray-700">Name</label>
                             <input type="text" name="name" id="name" class="form-input rounded-md shadow-sm mt-1 block w-full"
-                                   value="{{ old('name', $user->name) }}" />
+                                   value="{{ old('name', $loan->name) }}" />
                             @error('name')
                                 <p class="text-sm text-red-600">{{ $message }}</p>
                             @enderror
@@ -24,7 +24,7 @@
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <label for="email" class="block font-medium text-sm text-gray-700">Email</label>
                             <input type="email" name="email" id="email" class="form-input rounded-md shadow-sm mt-1 block w-full"
-                                   value="{{ old('email', $user->email) }}" />
+                                   value="{{ old('email', $loan->email) }}" />
                             @error('email')
                                 <p class="text-sm text-red-600">{{ $message }}</p>
                             @enderror
