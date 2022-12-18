9ff1db304d791199085502555b2f78950a472a39
franciscusvalentinus <120277436+franciscusvalentinus@users.noreply.github.com>
2022-12-18 20:45:15 +0700

    update view delete edit book

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -88,7 +88,6 @@
                                         </td>
 
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                            <a href="{{ route('books.edit', $book->id) }}" class="text-indigo-600 hover:text-indigo-900 mb-2 mr-2">Edit</a>
                                             <form class="inline-block" action="{{ route('books.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
                                                 <input type="hidden" name="_method" value="DELETE">
                                                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
